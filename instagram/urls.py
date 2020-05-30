301fed6a01b7b5ab0eedc182c4c1611c680d6745
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-30 21:31:13 +0300

    we configure our URLs to use the one step workflow for the registration

--- instagram/urls.py
+++ instagram/urls.py
@@ -19,4 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pics/', include('pics.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
