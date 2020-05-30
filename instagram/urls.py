bbeb278c904d68a08c382dea7606e36c6baf7d82
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-30 22:24:21 +0300

    We first import the views module from the django.contrib.auth module. Then we create a new URLconf. We provide the keyword argument next_page to define the page to go to after the user is logged out

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pics/', include('pics.urls')),
+    path('', include('pics.urls')),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', views.logout, {"next_page": '/'}), 
 ]
