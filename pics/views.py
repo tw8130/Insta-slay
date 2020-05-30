8574b8dfc07ad330e8f7a104c9c613ca6a3fdb90
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-30 22:09:26 +0300

    We use the @login_required decorator from the django.contrib.auth.decorators module then We pass in the the login_url argument that will redirect the unauthenticated user to the login route

--- pics/views.py
+++ pics/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required.
 
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def welcome(request):
-    return HttpResponse('Welcome to the Instagram lite')
+   return render (request,'welcome.html')
