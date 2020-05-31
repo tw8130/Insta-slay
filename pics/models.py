264144e967ca4912696db0d7afa598cafc60a3f2
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-31 22:36:27 +0300

    add changes to the image model and create follow,comments and profile models

--- pics/models.py
+++ pics/models.py
@@ -2,15 +2,44 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=150, null=True, blank=True)
+    profile_photo = models.ImageField(default='PHOTO', upload_to='profile_pics/')
+    # followers = models.ForeignKey(Follow, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.profile_photo 
+
+class Follow(models.Model):
+    following = models.ForeignKey(User, related_name="who_follows", on_delete=models.CASCADE)
+    follower = models.ForeignKey(User, related_name="who_is_followed",on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user 
+
 class Image(models.Model):
-    photo = models.ImageField(upload_to = "images/",null = True)
-    user = models.ForeignKey(User,null=True)
-    photo_name = models.CharField(max_length = 30,null = True)
+    photo = models.ImageField(upload_to = "images/",default='VALID PYTHON')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    photo_name = models.CharField(max_length = 30)
     likes = models.IntegerField(default=0)
-    photo_caption = models.TextField(null = True)
-    pub_date = models.DateTimeField(auto_now_add=True,null=True)
-    comments = models.TextField(default=0)
+    photo_caption = models.TextField(max_length=50)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile, null=True, on_delete=models.CASCADE)
+    photo_comments = models.IntegerField(default=0)
 
 
     def __str__(self):
-    	return self.pic_name
+    	return self.photo_name
+
+class Comments(models.Model):
+    text = models.TextField(max_length=255)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.ForeignKey(Image ,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
