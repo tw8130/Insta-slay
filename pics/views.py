3fca6e31dafcf0d5614910e0df2991ab568b1f29
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-06-01 18:19:40 +0300

    create a search results view function to display a searched user with their profile

--- pics/views.py
+++ pics/views.py
@@ -13,7 +13,7 @@ def welcome(request):
    all_users = Profile.objects.all()
    next = request.GET.get('next')
    if next: return redirect(next)
-   return render (request,'welcome.html' ,{'all_images':all_images, 'all_users':all_users})
+   return render (request,'all-pics/welcome.html' ,{'all_images':all_images, 'all_users':all_users})
 
 @login_required(login_url='/accounts/login/')
 def upload(request):
@@ -24,12 +24,12 @@ def upload(request):
         form = PostForm(request.POST,request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
-            post.imageuploader_profile= p
+            post.user= p
             post.save()
             return redirect('/')
     else:
         form =PostForm
-    return render(request, 'all_pics/upload.html', {"form": form})
+    return render(request, 'all-pics/upload.html', {"form": form})
 
 #Login page view function
 def login(request):
@@ -39,4 +39,14 @@ def login(request):
 def logout(request):
     return render(request, 'django_registration/logout.html')
 
+def search_results(request):
+    if 'profile' in request.GET and request.GET["profile"]:
+        search_term = request.GET.get("profile")
+        searched_profiles = Profile.search_by_profile(search_term)
+        message = f"{search_term}"
 
+        return render(request, 'search_pic.html',{"message":message,"profile": searched_profiles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search_pic.html',{"message":message})
