3c61367c4f2e16cbfa1d848efc97e3a0da99289d
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-06-01 11:15:32 +0300

    in the welcome view function we add the image and title variable to display in our template

--- pics/views.py
+++ pics/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required.
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def welcome(request):
-   return render (request,'welcome.html')
+   title = 'PetyInstagram'
+   image_posts = Image.objects.all()
+   return render (request,'welcome.html' ,{'title':title, 'image_posts':image_posts})
